c60059f1fa9ade88ea489fc22c5131f980e29adb
OmarKhled <74854173+OmarKhled@users.noreply.github.com>
2021-01-29 15:24:13 +0200

    Add files via upload
    Responsive design drop menu for mobiles

--- assets/navbar.php
+++ assets/navbar.php
@@ -6,12 +6,25 @@
     <div class="container">
       <h1> <a href="index.php"><strong>THE BLOOD BANK</strong></a></h1>
       <ul>
-        <li><a href="../index.php"><?php echo $lang['Home'] ?></a></li>
-        <li><a href="../about.php"><?php echo $lang['About'] ?></a></li>
-        <li><a href="../contact.php"><?php echo $lang['Contact'] ?></a></li>
-        <li><a class="language" href="index.php?lang=ar"><img src="../images/arabic.png" alt="<?php echo $lang['arabic'] ?>"></a></li>
-        <li><a class="language" href="index.php?lang=en"><img src="../images/english.png" alt="<?php echo $lang['english'] ?>"></a></li>
+        <span class="navigation">
+          <li class="floated"><a href="../index.php"><?php echo $lang['Home'] ?></a></li>
+          <li class="floated"><a href="../about.php"><?php echo $lang['About'] ?></a></li>
+          <li class="floated"><a href="../contact.php"><?php echo $lang['Contact'] ?></a></li>
+        </span>
+        <div class="floated m-navigation">
+          <a href="#">&#9776;</a>
+          <div class="drop-menu">
+            <li><a href="../index.php"><?php echo $lang['Home'] ?></a></li>
+            <li><a href="../about.php"><?php echo $lang['About'] ?></a></li>
+            <li><a href="../contact.php"><?php echo $lang['Contact'] ?></a></li>
+          </div>
+        </div>
+        <span>
+          <li class="floated"><a class="language" href="index.php?lang=ar"><img src="../images/arabic.png" alt="<?php echo $lang['arabic'] ?>"></a></li>
+          <li class="floated"><a class="language" href="index.php?lang=en"><img src="../images/english.png" alt="<?php echo $lang['english'] ?>"></a></li>
+        </span>
       </ul>
+
     </div>
 
   </nav>
